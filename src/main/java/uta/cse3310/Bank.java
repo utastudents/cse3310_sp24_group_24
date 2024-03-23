512c00d538f997937340381670554bf3325cf6c2
Nancy <ene6988@mavs.uta.edu>
2024-03-22 19:51:13 -0500

    3 new files

---
+++ src/main/java/uta/cse3310/Bank.java
@@ -0,0 +1,31 @@
+package uta.cse3310;
+import javio.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class bank
+{
+    public String checklength
+    {
+
+    }
+
+    public void readfile(String file)
+    {
+
+    
+    }
+    
+    public List<String> getWords(){
+        List<String> gamelist = new ArrayList<>();
+        // Add Stuff 
+
+        return gamelist;
+    }
+    public List<String> words()
+    {
+
+    }
+
+
+}
---
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -0,0 +1,21 @@
+package uta.cse3310;
+
+public class leaderboard
+{
+    public Leaderboard()
+    {
+        // player player = new leaderboard p
+
+    }
+
+    public void addPlayer(String Name, int score) // Add player with their score
+    {
+
+    }
+
+    public List<player> sorted(int x) // Sort the player
+    {
+
+    }
+
+}
