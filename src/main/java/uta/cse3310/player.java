d59b8d5339bd08b1bf370390fb21e8e3090a8e17
Robert Eberle <123016766+TresEberle@users.noreply.github.com>
2024-03-22 19:02:38 -0500

    adding player class

---
+++ src/main/java/uta/cse3310/player.java
@@ -0,0 +1,36 @@
+package uta.cse3310;
+
+public class player {
+
+    //Constructor
+    public player(){
+
+    }
+
+    //Player's Name
+    private String name;
+
+    //Player's Points Total
+    private int points;
+
+    //Player's Color
+    private String color;
+
+    //Getter for player name
+    public String getName(){
+
+        return "players name";
+    }
+
+    //Getter for player points
+    public int getPoints(){
+
+        return 1;
+    }
+    //Getter for player color
+    public String getColor(){
+
+        return "player color";
+    }
+
+}
