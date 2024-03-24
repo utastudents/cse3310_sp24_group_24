2653478ca39adf268f39842a589cd985b8d1cd85
Chime-txt <jxn1344@mavs.uta.edu>
2024-03-24 13:27:46 -0500

    Added Grid.java with functions from class diagram.

---
+++ src/main/java/uta/cse3310/Grid.java
@@ -0,0 +1,38 @@
+package uta.cse3310;
+
+public class Grid {
+    // Determines The Size Of Grid
+    /*
+     * Note 1: 50x50 Grid Might Be Too Large Of A Board
+     * Note 2: While The Variables Are Private (-) Based
+     * Off The
+     */
+    private int length, width;
+    private int boarderwidth, boarderlength;
+    private Bank Usedwords;
+
+    public Grid() {
+
+    }
+
+    public void fillwords(final : Bank) {
+
+    }
+
+    public void fillLetters() {
+
+    }
+
+    public int densityCheck() {
+
+    }
+
+    public int orientationCheck() {
+
+    }
+
+    public String FillerLettersRatio() {
+
+    }
+
+}
