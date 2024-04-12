18c6382e85d127152b772e47f8a83d1a632d7c8e
Nancy <ene6988@mavs.uta.edu>
2024-04-12 18:01:15 -0500

    Source code added

--- src/main/java/uta/cse3310/Leaderboard.java
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -1,25 +1,49 @@
 package uta.cse3310;
+
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class Leaderboard
-{
-    public Leaderboard()
-    {
-        // player player = new leaderboard p
+public class Leaderboard {
+    private List<Player> players;
 
+    public Leaderboard() {
+        this.players = new ArrayList<>();
     }
 
-    public void addPlayer(String Name, int score) // Add player with their score
-    {
-
+    public void addPlayer(String name, int score) {
+        Player newPlayer = new Player(name, score);
+        players.add(newPlayer);
+        sortPlayers();
     }
 
-    List<player> sorted(int x) // Sort the player
-    {
-        List<player> oneone = new ArrayList<>();
-        return oneone;
+    // You might also need a method to update a player's score
+    public void updateScore(String name, int newScore) {
+        for (Player player : players) {
+            if (player.getName().equals(name)) {
+                player.setScore(newScore);
+                break;
+            }
+        }
+        sortPlayers();
     }
-        
 
+    private void sortPlayers() {
+        Collections.sort(players, new Comparator<Player>() {
+            @Override
+            public int compare(Player p1, Player p2) {
+                return Integer.compare(p2.getScore(), p1.getScore()); // Descending order
+            }
+        });
+    }
+
+    public List<Player> getSortedPlayers() {
+        return players;
+    }
+
+    //Reset the leaderboard for a new game
+    public void resetLeaderboard() {
+        players.clear();
+    }
 }
