f4d807ef8a9281376bebfa664fbd60dc12146bad
Duy Tran <deeuyt001@gmail.com>
2024-04-13 02:24:58 -0500

    added test for bank

---
+++ src/test/java/uta/cse3310/banktest.java
@@ -0,0 +1,71 @@
+package uta.cse3310;
+
+import java.io.*;
+import java.util.*;
+
+public class banktest {
+
+    public static void main(String[] args){
+
+        System.out.println("working...");
+
+        double density,charCount,grid,check;
+        density = 0.67;
+        grid= 50 * 50;
+        List<String> wordlist = new  ArrayList<>();
+
+        String copy,filename;
+        filename="words.txt";
+        
+        //open file
+        try{
+            File wordfile = new File(filename);
+            Scanner input = new Scanner (wordfile);
+            
+            //add words to list
+            while(input.hasNextLine()){
+                copy = input.next();
+                wordlist.add(copy);
+            }
+            input.close();
+        }catch(Exception e){}
+
+        /*
+        for (String t : wordlist){
+            System.out.println(t);
+        }
+         */
+
+        charCount=0;
+        check=0;
+        Random picker = new Random();
+        List<String> newList = new  ArrayList<>();
+
+        //density check
+        while(check<density){
+            String oneword = wordlist.get(picker.nextInt(wordlist.size()));
+            //newList.add(oneword);
+            
+            //if randomly choosen word already in list, pick new word
+            //counting letters of words added.
+            if(!newList.contains(oneword)){
+                newList.add(oneword);
+                for (int i = 0; i < oneword.length(); i++) {
+                    if (Character.isLetter(oneword.charAt(i))) {
+                        charCount++;
+                    }
+                }
+            }
+            
+
+            check=charCount/grid;
+
+        }
+
+        for(String b : newList){
+            System.out.println(b);
+        }
+       //System.out.println(charCount+","+check);
+
+    }    
+}
