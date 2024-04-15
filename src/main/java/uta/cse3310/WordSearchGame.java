dfa022fbc0c97964fba34d72ad86b0be4103e6fe
Estefany Perez <exp3465@mavs.uta.edu>
2024-04-15 15:57:31 -0500

    some changes

--- src/main/java/uta/cse3310/WordSearchGame.java
+++ src/main/java/uta/cse3310/WordSearchGame.java
@@ -1,22 +1,30 @@
 package uta.cse3310;
-
+/*
+Grid is array of buttons containing letters
+*/
 public class WordSearchGame{
     public PlayerType players;
+    private Bank listOfWords; //all words in grid
+    private ArrayList<String> wordsToFind = new ArrayList<String>(); //list of words that are left to find
+    private String word = ""; //the word user has formed by clicking
 
 
-    WordSearchGame(){
-
-    }
-    
-    public void hint(){
+    public WordSearchGame(){
+    //what does this consist of exactly
 
     }
 
     public void wordfound(){
+        //if word user formed are in the words left to find
+        if(wordsToFind.contains(word)){
+            //change color background
+        }
+        wordsToFind.remove(word); //if found remove word from words to find
 
-    }
-
-    public void hintTimer(){
+        //if no more words left to find
+        if(wordsToFind.size() == 0){
+            //game ends, declare winner and exit back to main page
+        }
 
     }
 
