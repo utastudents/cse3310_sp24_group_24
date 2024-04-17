967525763128fef0c86d5ea0d7d1a215e41fd7a1
Estefany Perez <exp3465@mavs.uta.edu>
2024-04-17 13:56:47 -0500

    updates

--- src/main/java/uta/cse3310/WordSearchGame.java
+++ src/main/java/uta/cse3310/WordSearchGame.java
@@ -1,48 +1,48 @@
 package uta.cse3310;
 import java.util.ArrayList;
-/*
-Grid is array of buttons containing letters
-Words made up of buttons 
-*/
+
 public class WordSearchGame{
-    public PlayerType players;
-    private Bank listOfWords; //all words in grid
-    private ArrayList<String> wordsToFind = new ArrayList<String>(); //list of words that are left to find
-    private String word = ""; //the word user has formed by clicking
+    public PlayerType Players;
+    public Bank listOfWords; //all words in grid
+    public ArrayList<String> wordsToFind = new ArrayList<String>(); //list of words that are left to find
+    public String word = ""; //the word user has formed by clicking
+    public String[] Msg;
+    public Statistics S; 
+    public int numOfWords;
 
-    private boolean foundWord;
-    private int xPos; //row button is on
-    private int yPos; //col button is on
-
-
-    public WordSearchGame(){
-    //
-
+    public WordSearchGame(Statistics sta){
+        S = sta;
+        Msg = new String[2];
+        //S.GamesInProgress++; 
+        Players = PlayerType.XPLAYER;
+        numOfWords = wordsToFind.size();
+        Msg[0] = "Waiting for other player to join";
+        Msg[1] = "";
     }
 
+    public int PlayerToIdx(PlayerType P) {
+        int retval = 0;
+        if (P == PlayerType.XPLAYER) {
+            retval = 0;
+        } else {
+            retval = 1;
+        }
+        return retval;
+    }
     public void wordfound(){
         //if word user formed are in the words left to find
         if(wordsToFind.contains(word)){
-            
+            wordsToFind.remove(word); //if found remove word from words to find
+            numOfWords--;
+            Msg[0] = numOfWords + "words left to find!";
+            Msg[1] = numOfWords + "words left to find!";
         }
-        wordsToFind.remove(word); //if found remove word from words to find
-
         //if no more words left to find
         if(wordsToFind.size() == 0){
             //game ends, declare winner and exit back to main page
+            Msg[0] = "Game Over!";
+            Msg[1] = "Game Over!";
         }
-
     }
-
-    //user selects word by via clicking operation
-    public void clickOperation(){
-
-        
-    }
-    public void setFoundWord(boolean foundWord) {
-		this.foundWord = foundWord;
-		if(foundWord){}
-			//set color to players color
-	}
 
 }
