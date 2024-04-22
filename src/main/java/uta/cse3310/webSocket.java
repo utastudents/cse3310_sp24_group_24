d6d4ebd9d26fe81489d07f8ce7dbd54bf7c8521f
doublecode07 <158844696+doublecode07@users.noreply.github.com>
2024-04-22 18:38:06 -0500

    weebsocket

--- src/main/java/uta/cse3310/webSocket.java
+++ src/main/java/uta/cse3310/webSocket.java
@@ -1,5 +1,38 @@
 package uta.cse3310;
 
-public class webSocket {
+import javax.websocket.*;
+import java.io.*;
 
+public class webSocket {
+    int port;                      
+    Connection conn; 
+
+    Public webSocket(){
+
+    }
+
+    @OnStart
+    Public void start(){
+
+    }
+
+    @OnOpen
+    Public void on_open() throws IOException{
+
+    }
+
+    @OnMessage
+    Public void on_message() throws IOException{
+
+    }
+
+    @OnClose
+    Public void on_close() throws IOException{
+
+    }
+
+    @OnError
+    Public void on_error(){
+
+    }
 }
