c6b0e14df151ed8a74cb048b5b2925c981699828
Robert Eberle <123016766+TresEberle@users.noreply.github.com>
2024-04-22 16:46:06 -0500

    Add files via upload

---
+++ cse3310_sp24_group_24-main/src/main/java/uta/cse3310/Leaderboard.java
@@ -0,0 +1,25 @@
+package uta.cse3310;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Leaderboard
+{
+    public Leaderboard()
+    {
+        // player player = new leaderboard p
+
+    }
+
+    public void addPlayer(String Name, int score) // Add player with their score
+    {
+
+    }
+
+    List<Player> sorted(int x) // Sort the player
+    {
+        List<Player> oneone = new ArrayList<>();
+        return oneone;
+    }
+        
+
+}
