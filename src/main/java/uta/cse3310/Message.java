122ebc1dc177e052160cfcac51c94101f128512e
Robert Eberle <123016766+TresEberle@users.noreply.github.com>
2024-04-29 13:27:43 -0500

    Add files via upload
    adding grid creation

--- src/main/java/uta/cse3310/Message.java
+++ src/main/java/uta/cse3310/Message.java
@@ -1,6 +1,13 @@
 package uta.cse3310;
 
+import java.util.List;
+
 public class Message {
+    private String type;
+    private String message;
+    private char[][] grid;
+    private List<String> WordsUsed;
+    private int FoundWordIndex;
     private String sender;
     private String content;
 
@@ -10,11 +17,29 @@ public class Message {
     }
 
     public Message(Player sender, String content) {
-        
         this.sender = sender.getName();
         this.content = content;
     }
 
+    //Grid Message
+    public Message(char[][] grid){
+        this.type = "Grid";
+        this.grid = grid;
+    }
+
+    //Word Bank Message
+    public Message(List<String> WordsUsed){
+        this.type = "WordBank";
+        this.WordsUsed = WordsUsed;
+    }
+
+    //Found Word
+    public Message(int FoundWordIndex){
+        this.type = "FoundWord";
+        this.FoundWordIndex = FoundWordIndex;
+
+    }
+    
     public String getSender() {
         return sender;
     }
